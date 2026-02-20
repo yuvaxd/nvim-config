556d76edeee4b14ed0979f11b9227250c5b99c38
Tharun K <tharun9832@gmail.com>
2026-02-20 11:20:49 +0530

    Fix label_description issue in blink.cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -15,7 +15,21 @@ return {
 
     completion = {
       documentation = { auto_show = false },
-      menu = { draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } } } }
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 }
+          },
+          -- Fix label_description not showing up in the completion menu
+          components = {
+            label_description = {
+              text = function(ctx)
+                return ctx.label_description ~= '' and ctx.label_detail or ctx.item.detail
+              end
+            }
+          }
+        }
+      }
     },
 
     signature = { enabled = true },
