03dcd760d9dd61a03d7c1895d7830e0f5cf1cf40
Tharun K <tharun9832@gmail.com>
2026-04-25 15:31:06 +0530

    Change notification location from NE to SE

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -2,6 +2,13 @@ return {
   'nvim-mini/mini.notify',
   version = false,
   config = function()
-    require('mini.notify').setup()
+    local win_config = function()
+      local has_statusline = vim.o.laststatus > 0
+      local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+      return { anchor = 'SE', col = vim.o.columns, row = vim.o.lines - pad }
+    end
+    require('mini.notify').setup({
+      window = { config = win_config },
+    })
   end
 }
