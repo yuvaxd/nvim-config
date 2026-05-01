b4e9f41ca350dd41f118e5a18e8ea4bea667f8e4
Tharun K <tharun9832@gmail.com>
2026-05-01 20:00:58 +0530

    Update plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000,
+  "rose-pine/neovim",
+  name = "rose-pine",
   config = function()
-    require('onedark').setup {
-      transparent = true
-    }
-    require('onedark').load()
+    require("rose-pine").setup({
+      styles = {
+        transparency = true
+      }
+    })
+    vim.cmd("colorscheme rose-pine")
   end
 }
